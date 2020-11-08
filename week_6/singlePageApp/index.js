8391514279ba55957af05a0b66a3f0aec06d62b2
lambmikkel <66314737+lambmikkel@users.noreply.github.com>
2020-11-08 08:04:10 -0700

    update

--- week_6/singlePageApp/index.js
+++ week_6/singlePageApp/index.js
@@ -1,17 +1,66 @@
-const swapiData = document.querySelector("#swapiData");
+const main = document.getElementById("main")
 
-const swapiButton = document.querySelector("#swapiButton");
+async function getData() {
+    try {
+        const response = await axios.get("https://swapi.dev/api/people/")
+        listPerson(response)
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
 
-function getSwapiData() {
-    axios.get("https://swapi.dev/api/people/?")
-    .then(response => {
-        swapiData.innerHTML = response.data.results.map(function(item) {
-            return `<p>Name: ${Object.values(item)[0]}<br>
-            Height: ${Object.values(item)[1]}<br>
-            Gender: ${Object.values(item)[7]}</p>`
-        }).join('')
-    })
-    .catch(err => alert(err))
+getData()
+
+async function listPerson(response) {
+    for (let i = 0; i < response.data.results.length; i++) {
+
+        const listItem = document.createElement("div")
+        listItem.setAttribute("class", "listItem")
+        const h2 = document.createElement("h2")
+        h2.style.color = "lightblue"
+        const h4 = document.createElement("h4")
+        h4.style.color = "violet"
+
+        h2.textContent = `Name: ${response.data.results[i].name}`
+
+        const homeworld = await axios.get(response.data.results[i].homeworld)
+        const species = await axios.get(response.data.results[i].species)
+        const vehicles = response.data.results[i].vehicles
+        const starships = response.data.results[i].starships
+
+        h4.innerHTML = `Homeworld: ${homeworld.data.name}
+        <br>Species: ${species.data.name}`
+
+        listVehicles(vehicles, listItem)
+        listStarships(starships, listItem)
+
+        main.appendChild(listItem)
+        listItem.appendChild(h2)
+        listItem.appendChild(h4)
+    }
+
+    async function listVehicles(vehicles, listItem) {
+        for (let v = 0; v < vehicles.length; v++) {
+            const h4 = document.createElement("h4")
+            h4.style.color = "lightgreen"
+            const characterVehicles = await axios.get(vehicles[v])
+            h4.innerHTML = `Vehicles: ${characterVehicles.data.name}
+            <br>Model: ${characterVehicles.data.model}`
+            listItem.appendChild(h4)
+        }
+    }
+
+    async function listStarships(starships, listItem) {
+        for (let s = 0; s < starships.length; s++) {
+            const h4 = document.createElement("h4")
+            h4.style.color = "cyan"
+            const characterStarships = await axios.get(starships[s])
+            console.log(characterStarships)
+            h4.innerHTML = `Starships: ${characterStarships.data.name}
+            <br>Model: ${characterStarships.data.model}`
+            listItem.appendChild(h4)
+        }
+    }
 
 }
-swapiButton.addEventListener("click", getSwapiData);
